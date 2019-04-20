f40ef5e8247ff2335f46d4759311791552ed8ec3
anas-aljundi <anas.algendi@gmail.com>
2019-04-20 10:28:58 +0400

    Initial Commit

---
+++ API/Controllers/ValuesController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+
+        }
+        // GET api/values
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+
+            var values = await _context.Values.ToListAsync();
+            return Ok(values);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            var value = await _context.Values.SingleOrDefaultAsync(c => c.ValueId == id);
+            if(value == null){
+                return NotFound("please try again");
+            }
+            return Ok(value);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public async Task<IActionResult> PostValue([FromBody] Value value)
+        {
+            if (value.Name == null){
+                return BadRequest("Please Try Again");
+            }
+            await _context.Values.AddAsync(value);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}

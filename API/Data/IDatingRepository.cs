3e7d3eca1fcc5ec92dbaf011db8b3d3e1e1ada91
anas-aljundi <anas.algendi@gmail.com>
2019-05-23 17:10:22 +0400

    Pagination, Sorting and Filtering

--- API/Data/IDatingRepository.cs
+++ API/Data/IDatingRepository.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using API.Helpers;
 using API.Models;
 
 namespace API.Data
@@ -12,7 +13,7 @@ namespace API.Data
 
          Task<bool> SaveAll();
 
-         Task<IEnumerable<User>> GetUsers();
+         Task<PagedList<User>> GetUsers(UserParams userParams);
 
          Task<User> GetUser(int id);
          Task<Photo> GetPhoto(int id);
